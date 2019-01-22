838186c0de9e6e257e39338f2fedba35ebf0b410
massuu <dmasu0123@gmail.com>
2019-01-22 15:17:33 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,64 +1,35 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
-  def index
-    @tasks = Task.all.page(params[:page])
-  end
-
-  def show
-  end
-  
-  def new
-    @task = Task.new
-  end
+  before_action :correct_user, only: [:destroy]
 
   def create
     @task = current_user.tasks.build(task_params)
-   # @task = Task.new(task_params)
-
     if @task.save
-      flash[:success] = 'Task が正常に登録されました'
-      redirect_to @task
+      flash[:success] = 'Taskを登録しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が登録されませんでした'
-      render :new
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'Taskの登録に失敗しました。'
+      render 'toppages/index'
     end
   end
-  
-  def edit
-  end
-  
-  def update
-    if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Task は更新されませんでした'
-      render :edit
-    end
-  end
-  
+
   def destroy
     @task.destroy
-
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'Taskを削除しました。'
+    redirect_back(fallback_location: root_path)
   end
-  
-   private
 
-  # Strong Parameter
-  
-  def set_task
+  private
+
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+
+  def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to root_url
     end
   end
-  
-  def task_params
-    params.require(:task).permit(:content, :status)
-  end
-
 end
--- db/migrate/20190113134704_add_user_to_tasks.rb
+++ db/migrate/20190113134704_add_user_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddUserToTasks < ActiveRecord::Migration[5.0]
   def change
-    add_reference :tasks, :user, foreign_key: true
+    add_reference :tasks, :status, :user, foreign_key: true
   end
 end
